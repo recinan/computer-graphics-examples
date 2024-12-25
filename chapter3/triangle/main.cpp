9b619d4eb1f34e39dce411965288b4596c4c50a9
Recep Inan <recinan.ceng@gmail.com>
2024-12-25 21:45:59 +0300

    examples

---
+++ chapter3/triangle/main.cpp
@@ -0,0 +1,69 @@
+#include <GL/gl.h>
+#include <GL/glaux.h>
+
+int main(int argc, char** argv)
+{
+   auxInitDisplayMode (AUX_SINGLE | AUX_RGBA);
+   auxInitPosition (0, 0, 500, 500);
+   auxInitWindow (argv[0]);
+
+   glClearColor (0.0, 0.0, 0.0, 0.0);
+   glClear(GL_COLOR_BUFFER_BIT);
+   glColor3f(1.0, 1.0, 1.0);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   glOrtho(-5.0, 5.0, -5.0, 5.0, -5.0, 5.0);
+   
+   glLineWidth(2);
+   glBegin(GL_LINES);
+   		glVertex2f(-6.0,0.0);
+   		glVertex2f(6.0,0.0);
+   		glVertex2f(0.0,6.0);
+   		glVertex2f(0.0,-6.0);
+	glEnd();
+	
+	glPolygonMode(GL_BACK,GL_LINE);
+	glColor3f(0.0,0.0,1.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(3.0,3.0);
+		glVertex2f(5.0,3.0);
+		glVertex2f(3.0,4.0);
+	glEnd();
+	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(-4.0,-3.0,0.0); // move to origin (0.0,0.0)
+	glColor3f(1.0,0.0,0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(3.0,3.0);
+		glVertex2f(5.0,3.0);
+		glVertex2f(3.0,4.0);
+	glEnd();
+	
+	glLoadIdentity();
+	glRotatef(90.0,0.0,0.0,1.0); // rotation at origin
+	glTranslatef(-4.0,-3.0,0.0); // move to origin (0.0,0.0)
+	glColor3f(1.0,0.0,0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(3.0,3.0);
+		glVertex2f(5.0,3.0);
+		glVertex2f(3.0,4.0);
+	glEnd();
+	
+	glLoadIdentity();
+	glTranslatef(4.0,3.0,0.0);   // move to first place
+	glRotatef(90.0,0.0,0.0,1.0); // rotation at origin
+	glTranslatef(-4.0,-3.0,0.0); // move to origin (0.0,0.0)
+	glColor3f(1.0,0.0,0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(3.0,3.0);
+		glVertex2f(5.0,3.0);
+		glVertex2f(3.0,4.0);
+	glEnd();
+      
+   glFlush();
+
+   Sleep(3000);
+   return 0;
+}
+
---
+++ chapter3/triangle2/main.cpp
@@ -0,0 +1,140 @@
+#include <GL/gl.h>
+#include <GL/glaux.h>
+
+int main(int argc, char** argv)
+{
+   auxInitDisplayMode (AUX_SINGLE | AUX_RGBA);
+   auxInitPosition (0, 0, 500, 500);
+   auxInitWindow (argv[0]);
+
+   glClearColor (0.0, 0.0, 0.0, 0.0);
+   glClear(GL_COLOR_BUFFER_BIT);
+   glColor3f(1.0, 1.0, 1.0);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   glOrtho(-12.0, 12.0, -12.0, 12.0, -12.0, 12.0);
+   
+   glLineWidth(2);
+   glBegin(GL_LINES);
+   		glVertex2f(-12.0,0.0);
+   		glVertex2f(12.0,0.0);
+   		glVertex2f(0.0,12.0);
+   		glVertex2f(0.0,-12.0);
+	glEnd();
+	
+	glPolygonMode(GL_BACK,GL_LINE);
+	glColor3f(1.0,0.0,0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(2.0,1.0);
+		glVertex2f(3.0,1.0);
+		glVertex2f(3.0,2.0);
+	glEnd();
+	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glColor3f(0.0,1.0,0.0);
+	glTranslatef(-2.0,-2.0,0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(2.0,1.0);
+		glVertex2f(3.0,1.0);
+		glVertex2f(3.0,2.0);
+	glEnd();
+	
+	glLoadIdentity();
+	glColor3f(0.0,0.0,1.0);
+	glRotatef(90.0,0.0,0.0,1.0);
+	glTranslatef(-2.0,-2.0,0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(2.0,1.0);
+		glVertex2f(3.0,1.0);
+		glVertex2f(3.0,2.0);
+	glEnd();
+	
+	glLoadIdentity();
+	glColor3f(1.0,1.0,0.0);
+	glTranslatef(2.0,2.0,0.0);
+	glRotatef(90.0,0.0,0.0,1.0);
+	glTranslatef(-2.0,-2.0,0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(2.0,1.0);
+		glVertex2f(3.0,1.0);
+		glVertex2f(3.0,2.0);
+	glEnd();
+	
+	glLoadIdentity();
+	glColor3f(1.0,0.0,1.0);
+	glTranslatef(-2.0,-3.0,0.0);
+	glTranslatef(2.0,2.0,0.0);
+	glRotatef(90.0,0.0,0.0,1.0);
+	glTranslatef(-2.0,-2.0,0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(2.0,1.0);
+		glVertex2f(3.0,1.0);
+		glVertex2f(3.0,2.0);
+	glEnd();
+	
+	glLoadIdentity();
+	glColor3f(0.0,1.0,1.0);
+	glScalef(3.0,2.0,0.0);
+	glTranslatef(-2.0,-3.0,0.0);
+	glTranslatef(2.0,2.0,0.0);
+	glRotatef(90.0,0.0,0.0,1.0);
+	glTranslatef(-2.0,-2.0,0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(2.0,1.0);
+		glVertex2f(3.0,1.0);
+		glVertex2f(3.0,2.0);
+	glEnd();
+	
+	glLoadIdentity();
+	glColor3f(1.0,1.0,1.0);
+	glTranslatef(2.0,3.0,0.0);
+	glScalef(3.0,2.0,0.0);
+	glTranslatef(-2.0,-3.0,0.0);
+	glTranslatef(2.0,2.0,0.0);
+	glRotatef(90.0,0.0,0.0,1.0);
+	glTranslatef(-2.0,-2.0,0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(2.0,1.0);
+		glVertex2f(3.0,1.0);
+		glVertex2f(3.0,2.0);
+	glEnd();
+	
+	glLoadIdentity();
+	glColor3f(1.0,0.0,0.0);
+	glScalef(-1.0,1.0,1.0);
+	glTranslatef(2.0,3.0,0.0);
+	glScalef(3.0,2.0,0.0);
+	glTranslatef(-2.0,-3.0,0.0);
+	glTranslatef(2.0,2.0,0.0);
+	glRotatef(90.0,0.0,0.0,1.0);
+	glTranslatef(-2.0,-2.0,0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(2.0,1.0);
+		glVertex2f(3.0,1.0);
+		glVertex2f(3.0,2.0);
+	glEnd();
+	
+	glLoadIdentity();
+	glColor3f(0.0,1.0,0.0);
+	glTranslatef(-2.0,1.0,0.0);
+	glScalef(-1.0,1.0,1.0);
+	glTranslatef(2.0,3.0,0.0);
+	glScalef(3.0,2.0,0.0);
+	glTranslatef(-2.0,-3.0,0.0);
+	glTranslatef(2.0,2.0,0.0);
+	glRotatef(90.0,0.0,0.0,1.0);
+	glTranslatef(-2.0,-2.0,0.0);
+	glBegin(GL_TRIANGLES);
+		glVertex2f(2.0,1.0);
+		glVertex2f(3.0,1.0);
+		glVertex2f(3.0,2.0);
+	glEnd();
+	
+      
+   glFlush();
+
+   Sleep(9000);
+   return 0;
+}
+
